2b2ba55e0aff614cfb1884187f23f2e678c6bb41
masonyang <ystr20@qq.com>
2016-05-06 11:38:42 +0800

    域名修改

--- protected/classes/api/api.php
+++ protected/classes/api/api.php
@@ -11,7 +11,7 @@ class baseapi
 {
     protected $params = array();
 
-    const APIURL = 'http://192.168.1.100/';//192.168.1.100
+    const APIURL = 'http://a.qqcapp.com/';//192.168.1.100
 
     protected $output = array(
         'status'=>'fail',
