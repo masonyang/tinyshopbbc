35beb84f181a839fa072a4d8e7e6890889afcdee
masonyang <ystr20@qq.com>
2016-08-17 22:01:14 +0800

    fix

--- protected/classes/api/products.php
+++ protected/classes/api/products.php
@@ -215,14 +215,20 @@ class products extends baseapi
                             $spec_arr[$k]['name'] = $sval['name'];
                             $spec_arr[$k]['id'] = $sval['id'];
                             $spec_arr[$k]['value'] = $sval['value'][2];
+
+                            $freeze_nums = (($val['store_nums'] - $val['freeze_nums']) > 0) ? ($val['store_nums'] - $val['freeze_nums']) : 0;
+
                             $sys_attrprice[$pk] = array(
                                 'price'=>$val['branchstore_sell_price'] ? $val['branchstore_sell_price'] : $val['sell_price'],
                                 'pro_no'=>$val['pro_no'],
-                                'store_num'=>$val['store_nums'],
+                                'store_num'=>$freeze_nums,
                                 'name'=>$sval['name'],
                                 'value'=>$sval['value'][2],
                             );
                         }
+
+                        $freeze += $val['freeze_nums'];
+
                     }else{
                         $freeze += $val['freeze_nums'];
                     }
