4f476b416e5d48b45d2bfdbc862e9591bc1059f0
masonyang <ystr20@qq.com>
2016-08-22 14:52:20 +0800

    fix

--- protected/classes/api/paylink.php
+++ protected/classes/api/paylink.php
@@ -212,7 +212,7 @@ class paylink extends baseapi
                     //更新货品中的库存信息
                     $goods_nums = $pro['goods_nums'];
                     $product_id = $pro['product_id'];
-                    $productsModel->where("id=".$product_id)->data(array('freeze_nums'=>"`freeze_nums`+".$goods_nums))->update();
+                    $productsModel->where("id=".$product_id)->data(array('freeze_nums'=>"`freeze_nums`-".$goods_nums))->update();
                 }
 
                 $zdOrderModel = new Model('order','zd','master');
--- protected/controllers/order.php
+++ protected/controllers/order.php
@@ -646,7 +646,7 @@ class OrderController extends Controller
                             //更新货品中的库存信息
                             $goods_nums = $pro['goods_nums'];
                             $product_id = $pro['product_id'];
-                            $productsModel->where("id=".$product_id)->data(array('freeze_nums'=>"`freeze_nums`+".$goods_nums))->update();
+                            $productsModel->where("id=".$product_id)->data(array('freeze_nums'=>"`freeze_nums`-".$goods_nums))->update();
                         }
 
                         $orderInvoiceModel = new Model('order_invoice');
