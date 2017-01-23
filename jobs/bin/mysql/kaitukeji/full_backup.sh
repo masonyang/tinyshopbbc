031739f0b4f4b09895b84bca18e4a10aaf0e72be
masonyang <ystr20@qq.com>
2017-01-23 14:00:31 +0800

    fix

--- jobs/bin/mysql/kaitukeji/full_backup.sh
+++ jobs/bin/mysql/kaitukeji/full_backup.sh
@@ -12,8 +12,8 @@ Begin=`date +"%Y年%m月%d日 %H:%M:%S"`
 cd $BakDir
 DumpFile=$Date.sql
 GZDumpFile=$Date.sql.tgz
-/usr/local/bin/mysqldump -uqqc -pqqc --quick --all-databases --flush-logs --delete-master-logs --single-transaction > $DumpFile
-/usr/bin/tar czvf $GZDumpFile $DumpFile
+/usr/bin/mysqldump -uqqc -pqqc --quick --all-databases --flush-logs --delete-master-logs --single-transaction > $DumpFile
+/bin/tar czvf $GZDumpFile $DumpFile
 /bin/rm $DumpFile
 Last=`date +"%Y年%m月%d日 %H:%M:%S"`
 echo 开始:$Begin 结束:$Last $GZDumpFile succ >> $LogFile
